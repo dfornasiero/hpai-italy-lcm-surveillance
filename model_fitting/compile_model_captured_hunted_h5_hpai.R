7799b541e5d0944d7947a48b511893452cae2417
Diletta Fornasiero <96774712+dfornasiero@users.noreply.github.com>
2026-09-01 11:25:35 +0200

    Update compile_model_captured_hunted_h5_hpai.R

--- model_fitting/compile_model_captured_hunted_h5_hpai.R
+++ model_fitting/compile_model_captured_hunted_h5_hpai.R
@@ -65,4 +65,5 @@ fit <- model$sample(data = data_list,
                     iter_sampling = 2000,
                     parallel_chains = 4,
                     seed = 777)
-
+draws <- fit$draws()
+saveRDS(draws, "./draws_captured_hunted_h5_hpai.RDS")
